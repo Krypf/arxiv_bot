5e525eacec3c67979f29d6d6c8efbffa668bb913
Krypf <krypf2@gmail.com>
2024-08-01 05:53:02 +0900

    delete test_module

--- tests/test_example.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from ..arxiv_bot.example_module import hello_world
-
-class TestExampleModule(unittest.TestCase):
-    def test_hello_world(self):
-        self.assertEqual(hello_world(), "Hello, World!")
-
-if __name__ == "__main__":
-    unittest.main()
