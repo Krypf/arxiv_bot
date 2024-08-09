81db622967068f5972b8737f04230927c02d8743
Krypf <krypf2@gmail.com>
2024-08-09 13:08:01 +0900

    Rebuild index based on new .gitignore rules

--- tests/05.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class ArxivSpider(scrapy.Spider):
-    name = 'arxiv'
-    start_urls = ['https://arxiv.org/list/hep-th/recent']
-
-    def parse(self, response):
-        for title in response.css('.list-title'):
-            yield {'title': title.css('::text').get().strip()}
