87d8c9ca4f9199c6a7cc4690cc4954b1b8a63005
Krypf <krypf2@gmail.com>
2024-08-27 12:54:26 +0900

    Completed tests/twitter-test.py

--- arxiv_bot/post-twitter.py
+++ arxiv_bot/post-twitter.py
@@ -1,7 +1,11 @@
+#%%
 from twitter_function import load_credentials
 from arxiv_function import categories_content
 
 import tweepy
+import os
+
+#%%
 # Specify the file you want to read from
 category = credentials_file = categories_content[0]
 
@@ -9,34 +13,37 @@ category = credentials_file = categories_content[0]
 credentials_dict = load_credentials(credentials_file)
 
 # Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(
+client = tweepy.Client(
     consumer_key=credentials_dict['API Key'],
     consumer_secret=credentials_dict['API Key Secret'],
     access_token=credentials_dict['Access Token'],
     access_token_secret=credentials_dict['Access Token Secret']
 )
-
-# Create API object
-api = tweepy.API(auth)
-print(api)
 # Function to read content from a text file and tweet it
-def tweet_from_file(filename):
+def tweet_from_file(file_name):
+    parent_folder = 'tests'
+    file_path = os.path.join(parent_folder, file_name)
     try:
-        with open(filename, 'r') as file:
+        with open(file_path, 'r') as file:
             tweet_content = file.read().strip()
-            
+            print(tweet_content)
             # Check if the tweet content is within Twitter's character limit
             if len(tweet_content) <= 280:
-                api.update_status(tweet_content)
+                # Post Tweet
+                client.create_tweet(text=tweet_content)
                 print("Tweet posted successfully!")
             else:
                 print("Tweet content exceeds 280 characters. Please shorten the text.")
                 
     except FileNotFoundError:
-        print(f"File {filename} not found.")
-    except tweepy.TweepError as e:
-        print(f"Error occurred: {e.reason}")
+        print(f"File {file_path} not found.")
+
+    except tweepy.errors.TweepyException as e:
+        print(file_path)
+        print(f"Error occurred: {e}")
 
 # Specify the file you want to tweet from
 tweet_file = "tweet.txt"
 tweet_from_file(tweet_file)
+
+# %%
--- tests/twitter-test.py
+++ tests/twitter-test.py
@@ -18,7 +18,7 @@ category = credentials_file = categories_content[0]
 credentials_dict = load_credentials(credentials_file)
 
 # Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(
+client = tweepy.Client(
     consumer_key=credentials_dict['API Key'],
     consumer_secret=credentials_dict['API Key Secret'],
     access_token=credentials_dict['Access Token'],
@@ -26,29 +26,29 @@ auth = tweepy.OAuth1UserHandler(
 )
 
 # Create API object
-api = tweepy.API(auth)
-print(api)
+# api = tweepy.API(auth)
+# print(api)
 # Function to read content from a text file and tweet it
 def tweet_from_file(file_name):
     parent_folder = 'tests'
     file_path = os.path.join(parent_folder, file_name)
-    
     try:
         with open(file_path, 'r') as file:
             tweet_content = file.read().strip()
-            
+            print(tweet_content)
             # Check if the tweet content is within Twitter's character limit
             if len(tweet_content) <= 280:
-                api.update_status(tweet_content)
+                # Post Tweet
+                client.create_tweet(text=tweet_content)
                 print("Tweet posted successfully!")
             else:
                 print("Tweet content exceeds 280 characters. Please shorten the text.")
                 
     except FileNotFoundError:
-        print(f"File {file_name} not found.")
+        print(f"File {file_path} not found.")
 
     except tweepy.errors.TweepyException as e:
-        print(file_name)
+        print(file_path)
         print(f"Error occurred: {e}")
 
 # Specify the file you want to tweet from
