654e0c77b136b3309ed072ecfd6a0ef9bfd862e7
unknown <krypf2@gmail.com>
2024-09-15 15:05:48 +0900

    change run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e # Exit immediately if a command exits with a non-zero status
 PATH=$HOME/.pyenv/shims:$PATH
-# cd ~/arxiv_bot
-python arxiv_bot/arxiv-save-HTML.py
+python arxiv_bot/save-HTML.py
 sleep 1
-python arxiv_bot/arxiv-save-text.py
+python arxiv_bot/save-json.py
 sleep 1
 python arxiv_bot/post.py
-# echo "Done"
