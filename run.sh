be4c1a6a75d97f2d663a739482650946c98a6cd1
Krypf <krypf2@gmail.com>
2024-09-16 15:22:22 +0900

    changed run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e # Exit immediately if a command exits with a non-zero status
 PATH=$HOME/.pyenv/shims:$PATH
-python arxiv_bot/save-HTML.py
-sleep 0.5
-python arxiv_bot/save-json.py
-sleep 0.5
-python arxiv_bot/post.py
+python arxiv_bot/main.py
+# python arxiv_bot/save-HTML.py
+# sleep 0.5
+# python arxiv_bot/save-json.py
+# sleep 0.5
+# python arxiv_bot/post.py
