22f7689f737a2cf26ac32d2db5b90adfef85a1a9
Krypf <krypf2@gmail.com>
2024-10-17 04:38:43 +0900

    move post_bluesky() and rename post_bluesky.py

---
+++ src/post_bluesky.py
@@ -0,0 +1,25 @@
+#%%
+# https://chatgpt.com/share/2e11c4e2-96d3-4c1c-82a0-cbed39d6ec9a
+from src.utils.printlog import printlog
+from src.core.arxiv_function import ArxivText
+from src.utils.get_args import get_today, categories_content
+
+#%%
+def post_bluesky(today):
+    for category in reversed(categories_content):
+        reader = ArxivText(category, today, extension='.json')
+        reader.update_bluesky()
+        
+def main():
+    today = get_today()
+    
+    printlog('Start posting on Bluesky')
+    post_bluesky(today)
+    printlog(f"This is the end of all the posts on {today}")
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
+
+# %%
