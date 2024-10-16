0985c7bc6870f61d394d4853b510f228a5921026
Krypf <krypf2@gmail.com>
2024-10-17 06:19:03 +0900

    Remove save-json.py and save-HTML.py

--- src/save-HTML.py
+++
@@ -1,9 +0,0 @@
-#%% 
-from src.core.arxiv_function import ArxivSearch
-from src.utils.get_args import html_args, categories_content
-# from typing import Optional
-
-main = save_html
-#%%
-if __name__ == "__main__":
-    main()
--- src/save-json.py
+++
@@ -1,7 +0,0 @@
-#%%
-from src.core.arxiv_function import ArxivText
-from src.utils.get_args import get_today, categories_content
-
-main = save_json
-if __name__ == '__main__':
-    main()
