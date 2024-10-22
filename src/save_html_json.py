b92178ac65ad364e31fe03171fe8e86c3ed14cc1
Krypf <krypf2@gmail.com>
2024-10-23 08:49:04 +0900

    add _check=True and save_html_json_one_category

--- src/save_html_json.py
+++ src/save_html_json.py
@@ -1,21 +1,31 @@
 from src.utils.get_args import get_args, get_today, categories_content
 from src.core.arxiv_function import ArxivSearch, ArxivText
 #%%
-def save_html_json(today, args):
+def save_html_json(today, args, _check=True):
     # be going to change args
     for category in categories_content:
         # save_html
         search = ArxivSearch(category, args.submissions, args.skip, args.show)
-        search.save_one_html()
+        search.save_one_html(_check)
         # save_json
         data = ArxivText(category, today, extension='.json')
         data.save_one_json()
     return 0
+
+def save_html_json_one_category(today, args, _check=True):
+    # save_html
+    search = ArxivSearch(args.category, args.submissions, args.skip, args.show)
+    search.save_one_html(_check)
+    # save_json
+    data = ArxivText(args.category, today, extension='.json')
+    data.save_one_json()
+    return 0
+
 #%%
-def save_html(args):
+def save_html(args, _check=True):
     for category in categories_content:
         search = ArxivSearch(category, args.submissions, args.skip, args.show)
-        search.save_one_html()
+        search.save_one_html(_check)
     return 0
 
 def save_json(args):
