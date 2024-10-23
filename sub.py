511470bc09dabf77b487e9e7fcaafcf92137d9ba
Krypf <krypf2@gmail.com>
2024-10-24 06:17:21 +0900

    Reflesh sub.py

--- sub.py
+++ sub.py
@@ -6,14 +6,14 @@ from src.save_html_json import save_html_json
 from src.post_bluesky import post_bluesky
 from src.post_twitter import post_twitter
 
-def main():
+def sub():
     args = get_args()
     today = get_today(args)
-    # save_html_json(today, args)
-    # post_bluesky(today)
-    post_twitter(today, twi = 3)
+    save_html_json(today, args, _check=True)
+    post_bluesky(today)
+    post_twitter(today, twi = 5)
     printlog(f"This is the end of all the posts on {today}")
     return 0
 
 if __name__ == '__main__':
-    main()
+    sub()
