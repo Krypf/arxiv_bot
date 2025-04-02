879194257293a910963f3c6af2e74d1a9cf4df9c
Krypf <krypf2@gmail.com>
2025-04-02 15:35:01 +0900

    add sleep

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e # Exit immediately if a command exits with a non-zero status
-PATH=$HOME/.pyenv/shims:$PATH
+export PATH=$HOME/.pyenv/shims:$PATH
+# sh ~/auto/wifi/tethering.sh
+sleep 1
 python main.py
