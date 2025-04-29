02c56cbaf53f1520c79a9a6141070925cc4644d2
Krypf <krypf2@gmail.com>
2025-04-29 18:22:29 +0900

    delete set -e

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e # Exit immediately if a command exits with a non-zero status
+# set -e # Exit immediately if a command exits with a non-zero status
 export PATH=$HOME/.pyenv/shims:$PATH
 # sh ~/auto/wifi/tethering.sh
 sleep 1
