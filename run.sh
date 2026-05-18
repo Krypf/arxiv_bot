1b486de2ff2ae4fce2d43b8a51a43d2b8750283e
“Krypf” <krypf2@gmail.com>
2026-05-18 17:39:44 +0900

    add venv and starting shells

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # set -e # Exit immediately if a command exits with a non-zero status
-export PATH=$HOME/.pyenv/shims:$PATH
 # sh ~/auto/wifi/tethering.sh
+export PATH=$HOME/arxiv_bot/.venv/bin:$PATH
 sleep 1
 python main.py
+sleep 1
+sh shell/arxiv_start.zsh
